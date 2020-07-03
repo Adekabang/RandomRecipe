5229fb3d7b5b37b29dc7e91551937362ea27d0c6
Mohammad Raska <iban.ninja@gmail.com>
2020-07-04 01:17:49 +0700

    install axios

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,36 +1,38 @@
-import '../component/recipe-item.js'
+import "../component/recipe-item.js";
+const axios = require("axios");
 
 const main = () => {
-  const body = document.querySelector("body")
-  const randomBtn = document.querySelector("#random")
-  const mainText = document.querySelector("#main-text")
-  const loading = document.querySelector(".loading")
-  const recipe = document.querySelector("#recipe")
-  const baseUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
-  const imageUrl = "https://www.themealdb.com/images/ingredients/"
+  const body = document.querySelector("body");
+  const randomBtn = document.querySelector("#random");
+  const mainText = document.querySelector("#main-text");
+  const loading = document.querySelector(".loading");
+  const recipe = document.querySelector("#recipe");
+  const baseUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+  const imageUrl = "https://www.themealdb.com/images/ingredients/";
 
   randomBtn.addEventListener("click", () => {
-    mainText.innerHTML = "Please Wait"
-    loading.classList.remove('hidden')
-    randomBtn.classList.add("hidden")
-    getRecipe()
-  })
+    mainText.innerHTML = "Please Wait";
+    loading.classList.remove("hidden");
+    randomBtn.classList.add("hidden");
+    getRecipe();
+  });
 
   const getRecipe = () => {
-    fetch(`${baseUrl}`)
-      .then(response => {
-        return response.json();
+    axios
+      .get(`${baseUrl}`)
+      .then((response) => {
+        return response.data;
       })
-      .then(responseJson => {
+      .then((responseJson) => {
         if (responseJson.error) {
           showResponseMessage(responseJson.message);
         } else {
           renderRecipe(responseJson.meals[0]);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         showResponseMessage(error);
-      })
+      });
   };
 
   const showResponseMessage = (message = "Check your internet connection") => {
@@ -38,15 +40,15 @@ const main = () => {
   };
 
   const renderRecipe = (result) => {
-    body.classList.remove("full-height")
-    mainText.classList.add("hidden")
-    randomBtn.classList.remove("hidden")
-    loading.classList.add("hidden")
+    body.classList.remove("full-height");
+    mainText.classList.add("hidden");
+    randomBtn.classList.remove("hidden");
+    loading.classList.add("hidden");
 
-    recipe.innerHTML = ""
+    recipe.innerHTML = "";
     const recipeItem = document.createElement("recipe-item");
-    recipeItem.recipe = result
+    recipeItem.recipe = result;
     recipe.appendChild(recipeItem);
-  }
-}
+  };
+};
 export default main;
