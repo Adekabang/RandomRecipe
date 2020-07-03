41200b4b55806e7d0ebef0d3b5d45451ebe617b2
Mohammad Raska <iban.ninja@gmail.com>
2020-07-04 00:54:05 +0700

    update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/app.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     filename: "bundle.js"
   },
   module: {
